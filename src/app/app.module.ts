c63040aa57e81328535d6cc7a04f64156594d83f
nagamadhan_1 <nagamadhan_1@gmail.com>
2018-04-26 22:11:12 +0530

    warning comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {WarningAlertComponent} from "./warningalert/warningalert.component";
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningAlertComponent
   ],
   imports: [
     BrowserModule
