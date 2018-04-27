321f5795bd4e5558ed005fae055dcacd36017264
nagamadhan_1 <nagamadhan_1@gmail.com>
2018-04-27 11:04:14 +0530

    success

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {WarningAlertComponent} from "./warningalert/warningalert.component";
+import {SuccessAlertComponent} from './successalert/successalert.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    WarningAlertComponent
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule
